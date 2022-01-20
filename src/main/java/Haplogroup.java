10872c1ae508c81b212e1b622ff2df1a231b9294
Alexander Lukyanov <lux-nova@mail.ru>
2022-01-20 22:31:03 +0300

    Haplogroup init

---
+++ src/main/java/Haplogroup.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Haplogroup {
+    private final String notation;
+    private final String haplotype;
+    private final Haplogroup ancestor;
+    private List<Haplogroup> descendents;
+
+    public Haplogroup(String notation, String haplotype, Haplogroup ancestor) {
+        this.notation = notation;
+        this.haplotype = haplotype;
+        this.ancestor = ancestor;
+        descendents = new LinkedList<>();
+    }
+
+    public static Haplogroup rootGroup() {
+        return new Haplogroup("ROOT", "0", null);
+    }
+
+    public Haplogroup divide(String newName, String forType) {
+        Haplogroup newGroup = new Haplogroup(newName, forType, this);
+        descendents.add(newGroup);
+        return newGroup;
+    }
+}
